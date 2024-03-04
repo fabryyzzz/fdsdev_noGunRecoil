bac204020f2f8eedf6e9de34b9a64e80432df56d
Fabrizio - FDS Development <58892804+fabryyzzz@users.noreply.github.com>
2024-03-04 14:21:35 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,63 @@
+Config = {}
+Config.WeaponRecoil = {
+	[GetHashKey('WEAPON_PISTOL')] = {recoil = 0.0}, 	--PISTOL	
+	[GetHashKey('WEAPON_PISTOL_MK2')] = {recoil = 0.0}, 	--PISTOL MK2
+	[GetHashKey('WEAPON_COMBATPISTOL')] = {recoil = 0.0}, -- COMBAT PISTOL
+	[GetHashKey('WEAPON_APPISTOL')] = {recoil = 0.0}, -- AP PISTOL
+	[GetHashKey('WEAPON_PISTOL50')] = {recoil = 0.0}, -- PISTOL .50
+	[GetHashKey('WEAPON_MICROSMG')] = {recoil = 0.0}, -- MICRO SMG
+	[GetHashKey('WEAPON_SMG')] = {recoil = 0.0}, -- SMG
+	[GetHashKey('WEAPON_SMG_MK2')] = {recoil = 0.0}, -- SMG MK2
+	[GetHashKey('WEAPON_ASSAULTSMG')] = {recoil = 0.0}, -- ASSAULT SMG
+	[GetHashKey('WEAPON_ASSAULTRIFLE')] = {recoil = 0.0}, -- ASSAULT RIFLE
+	[GetHashKey('WEAPON_ASSAULTRIFLE_MK2')] = {recoil = 0.0},-- ASSAULT RIFLE MK2
+	[GetHashKey('WEAPON_CARBINERIFLE')] = {recoil = 0.0}, -- CARBINE RIFLE
+	[GetHashKey('WEAPON_CARBINERIFLE_MK2')] = {recoil = 0.0}, -- CARBINE RIFLE MK2
+	[GetHashKey('WEAPON_ADVANCED_RIFLE')] = {recoil = 0.0}, -- ADVANCED RIFLE
+	[GetHashKey('WEAPON_MG')] = {recoil = 0.0}, -- MG
+	[GetHashKey('WEAPON_COMBATMG')] = {recoil = 0.0}, -- COMBAT MG
+	[GetHashKey('WEAPON_COMBATMG_MK2')] = {recoil = 0.0},-- COMBAT MG MK2
+	[GetHashKey('WEAPON_PUMPSHOTGUN')] = {recoil = 0.0}, -- PUMP SHOTGUN
+	[GetHashKey('WEAPON_PUMPSHOTGUN_MK2')] = {recoil = 0.0},-- PUMP SHOTGUN MK2
+	[GetHashKey('WEAPON_SAWNOFFSHOTGUN')] = {recoil = 0.0}, -- SAWNOFF SHOTGUN
+	[GetHashKey('WEAPON_ASSAULTSHOTGUN')] = {recoil = 0.0}, -- ASSAULT SHOTGUN
+	[GetHashKey('WEAPON_BULLPUPSHOTGUN')] = {recoil = 0.0},-- BULLPUP SHOTGUN
+	[GetHashKey('WEAPON_STUNGUN')] = {recoil = 0.0},-- STUN GUN
+	[GetHashKey('WEAPON_SNIPERRIFLE')] = {recoil = 0.0},-- SNIPER RIFLE
+	[GetHashKey('WEAPON_HEAVYSNIPER')] = {recoil = 0.0},-- HEAVY SNIPER
+	[GetHashKey('WEAPON_HEAVYSNIPER_MK2')] = {recoil = 0.0}, -- HEAVY SNIPER MK2
+	[GetHashKey('WEAPON_REMOTESNIPER')] = {recoil  = 0.0},-- REMOTE SNIPER
+	[GetHashKey('WEAPON_GRENADELAUNCHER')] = {recoil = 0.0},-- GRENADE LAUNCHER
+	[GetHashKey('WEAPON_GRENADELAUCHER_SMOKE')] = {recoil = 0.0},-- GRENADE LAUNCHER SMOKE
+	[GetHashKey('WEAPON_RPG')] = {recoil = 0.0},-- RPG
+	[GetHashKey('WEAPON_STINGER')] = {recoil = 0.0},-- STINGER
+	[GetHashKey('WEAPON_MINIGUN')] = {recoil = 0.0},  -- MINIGUN
+	[GetHashKey('WEAPON_SNSPISTOL')] = {recoil = 0.0},-- SNS PISTOL
+	[GetHashKey('WEAPON_SNSPISTOL_MK2')] = {recoil = 0.0}, -- SNS PISTOL MK2
+	[GetHashKey('WEAPON_GUSENBERG')] = {recoil = 0.0}, -- GUSENBERG
+	[GetHashKey('WEAPON_SPECIALCARBINE')] = {recoil = 0.0},-- SPECIAL CARBINE
+	[GetHashKey('WEAPON_SPECIALCARBINE_MK2')] = {recoil = 0.0}, -- SPECIAL CARBINE MK2
+	[GetHashKey('WEAPON_HEAVYPISTOL')] = {recoil = 0.0},-- HEAVY PISTOL		--new hashkey
+	[GetHashKey('WEAPON_BULLPUPRIFLE')] = {recoil = 0.0},-- BULLPUP RIFLE
+	[GetHashKey('WEAPON_BULLPUPRIFLE_MK2')] = {recoil = 0.0}, -- BULLPUP RIFLE MK2
+	[GetHashKey('WEAPON_VINTAGEPISTOL')] = {recoil = 0.0}, -- VINTAGE PISTOL
+	[GetHashKey('WEAPON_DOUBLEACTION')] = {recoil = 0.0}, -- DOUBLE ACTION REVOLVER
+	[GetHashKey('WEAPON_MUSKET')] = {recoil = 0.0}, -- MUSKET
+	[GetHashKey('WEAPON_HEAVYSHOTGUN')] = {recoil = 0.0}, -- HEAVY SHOTGUN
+	[GetHashKey('WEAPON_MARKSMANRIFLE')] = {recoil = 0.0}, -- MARKSMAN RIFLE
+	[GetHashKey('WEAPON_MARKSMANRIFLE_MK2')] = {recoil = 0.0}, -- MARKSMAN RIFLE MK2
+	[GetHashKey('WEAPON_HOMINGLAUNCHER')] = {recoil = 0.0 }, -- HOMING LAUNCHER
+	[GetHashKey('WEAPON_FLAREGUN')] = {recoil = 0.0}, -- FLARE GUN
+	[GetHashKey('WEAPON_COMBATPDW')] = {recoil = 0.0}, -- COMBAT PDW
+	[GetHashKey('WEAPON_MARKSMANPISTOL')] = {recoil = 0.0}, -- MARKSMAN PISTOL
+  	[GetHashKey('WEAPON_RAILGUN')] = {recoil = 0.0}, -- RAILGUN
+	[GetHashKey('WEAPON_MACHINEPISTOL')] = {recoil = 0.0},-- MACHINE PISTOL
+	[GetHashKey('WEAPON_REVOLVER')] = {recoil = 0.0}, -- REVOLVER
+	[GetHashKey('WEAPON_REVOLVER_MK2')] = {recoil = 0.0}, -- REVOLVER MK2
+	[GetHashKey('WEAPON_DBSHOTGUN')] = {recoil = 0.0}, -- DOUBLE BARREL SHOTGUN
+	[GetHashKey('WEAPON_COMPACTRIFLE')] = {recoil = 0.0}, -- COMPACT RIFLE
+	[GetHashKey('WEAPON_AUTOSHOTGUN')] = {recoil = 0.0},-- AUTO SHOTGUN
+	[GetHashKey('WEAPON_COMPACTLAUNCHER')] = {recoil = 0.0}, -- COMPACT LAUNCHER
+	[GetHashKey('WEAPON_MINISMG')] = {recoil = 0.0}, -- MINI SMG
+	
+}
